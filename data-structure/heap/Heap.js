58b006fed574eba02d52dfe78b648303ab802c32
Alita <violetsshine@gmail.com>
2019-08-25 18:11:11 +0800

    feat: add heap

--- data-structure/heap/Heap.js
+++ data-structure/heap/Heap.js
@@ -2,8 +2,85 @@ import Comparator from '../utils/comparator/Comparator';
 
 export default class Heap {
   constructor(compareFunction) {
+    // use a array to mock heap
     this.heapContainer = [];
     this.compare = new Comparator(compareFunction);
+  }
+
+  /**
+   * parent 1 childL 3 childR 4
+   * 2n + 1
+   */
+  getLeftChildIndex(parentIndex) {
+    return (2 * parentIndex) + 1;
+  }
+
+  getRightChildIndex(parentIndex) {
+    return (2 * parentIndex) + 2;
+  }
+
+  /**
+   * childI 3 | 4 parent 1
+   *  (n - 1) / 2
+   */
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  /**
+   * [0, 1, 2, 3, 4]
+   * parent 1 < 5 true
+   * parent 3 < 5 false
+   */
+  hasLeftChild(parentIndex) {
+    return this.getLeftChildIndex(parentIndex) < this.heapContainer.length
+  }
+
+  hasRightChild(parentIndex) {
+    return this.getRightChildIndex(parentIndex) < this.heapContainer.length;
+  }
+
+  // top is -1
+  hasParent(childIndex) {
+    return this.getParentIndex(childIndex) >= 0;
+  }
+
+  leftChild(parentIndex) {
+    return this.heapContainer[this.getLeftChildIndex(parentIndex)];
+  }
+
+  rightChild(parentIndex) {
+    return this.heapContainer[this.getRightChildIndex(parentIndex)];
+  }
+
+  parent(childIndex) {
+    return this.heapContainer[this.getParentIndex(childIndex)];
+  }
+
+  swap(indexOne, indexTwo) {
+    const tmp = this.heapContainer[indexTwo];
+    this.heapContainer[indexTwo] = this.heapContainer[indexOne];
+    this.heapContainer[indexOne] = tmp;
+  }
+
+  // get the first one
+  peek() {
+    if (this.heapContainer.length === 0) {
+      return null;
+    }
+
+    return this.heapContainer[0];
+  }
+
+  poll() {
+    if (this.heapContainer.length === 0) {
+      return null;
+    }
+
+    if (this.heapContainer.length === 1) {
+      return this.heapContainer.pop();
+    }
+
   }
 
   add(item) {
@@ -12,23 +89,40 @@ export default class Heap {
     return this;
   }
 
+  remove(item, comparator = this.compare) {
+
+  }
+
+  find(item, comparator = this.compare) {
+    const foundIndex = [];
+    let index = 0;
+
+    for(; index < this.heapContainer.length; index++) {
+      if (comparator.equal(item, this.heapContainer[index])) {
+        foundIndex.push(index);
+      }
+    }
+
+    return foundIndex;
+  }
+
+  isEmpty() {
+    return !this.heapContainer.length;
+  }
+
+  toString() {
+    return this.heapContainer.toString();
+  }
+
   heapifyUp(customStartIndex) {
 
   }
 
-  hasParent() {
-    return this.getParentIndex(childIndex) >= 0;
+  heapifyDown(customStartIndex) {
+
   }
 
-  getParentIndex(childIndex) {
-    // len = 9; 8 / 4 = 2;
-    // len = 1; 0 / 2 = 0;
-    // len = 2; 1 / 2 = 0;
-    return Math.floor((childIndex - 1) / 2);
-  }
+  checkIsInCorrectOrder(a, b) {
 
-  // get parent item
-  parent(childIndex) {
-    return this.heapContainer[this.getParentIndex(childIndex)];
   }
 }
--- data-structure/heap/README.md
+++ data-structure/heap/README.md
@@ -12,3 +12,24 @@
 
 最大堆满足 P 为 C 的一个节点，P 始终大于 C的值
 ![example](https://camo.githubusercontent.com/cf3c66d0d2ed67af70a8bc500fc215526d266a0d/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f332f33382f4d61782d486561702e737667)
+
+
+## Reference 
+
+- [堆](https://www.jianshu.com/p/6b526aa481b1)
+
+
+## 特点
+
+1. 类似于树，但必须满足最小堆和最大堆
+2. 
+
+##　内部实现
+
+1. `heapContainer`: 使用数组来模拟堆结构
+2. `getLeftChildIndex(parentIndex)`：
+3. `getRightChildIndex(parentIndex)`：
+4. `getParentIndex(childIndex)`：
+5. `leftChild(parentIndex)`：
+6. `rightChild(parentIndex)`：
+7. `parent(childIndex)`：
