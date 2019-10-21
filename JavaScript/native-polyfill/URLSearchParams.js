558f87dccdb0f194e12e4611dd688246a02ff797
zZ <violetsshine@gmail.com>
2019-10-21 10:41:07 +0800

    feat: add URLSearchParams

---
+++ JavaScript/native-polyfill/URLSearchParams.js
@@ -0,0 +1,71 @@
+/**
+ * @author zhoux
+ * @since 20191021
+ * @description Chrome 74+
+ */
+
+class MyURLSearchParams {
+
+    #searchParams = [];
+
+    constructor(init) {
+        if (typeof init === 'string') {
+            this.#searchParams = init.split('&').map(kv => kv.split('='));
+        } else {
+            this.#searchParams = Object.entries(init);
+        }
+    }
+
+    get(key) {
+        const param = this.#searchParams.find(kv => kv[0] === key);
+        return param && param[1];
+    }
+
+    set(key, value) {
+        const param = this.#searchParams.find(kv => kv[0] === key);
+        if (param) {
+            param[1] = value;
+        } else {
+            this.append(key, value);
+        }
+    }
+
+    has(key) {
+        return this.#searchParams.some(kv => kv[0] === key);
+    }
+
+    append(key, value) {
+        this.#searchParams.push([key, value])
+    }
+    
+    toString() {
+        return this.#searchParams.map(kv => kv.join('=')).join('&')
+    }
+
+    *[Symbol.iterator]() {
+        yield* this.#searchParams;
+    }
+}
+
+// 构造函数支持传入 URL 参数串
+let searchParams = new MyURLSearchParams("foo=11&bar=22");
+
+// 构造函数也支持传入一个包含参数键值对的对象
+searchParams = new MyURLSearchParams({foo: "1", bar: "2"})
+
+// 实例支持 get()、set()、has()、append() 四个方法
+console.log(searchParams.get("foo")) // "1"
+searchParams.set("foo", "10") 
+console.log(searchParams.has("bar")) // true
+searchParams.append("foo", "100") 
+
+// 实例支持 toString() 方法
+console.log(searchParams.toString()) // "foo=10&bar=2&foo=100"
+
+// 实例支持 for-of 迭代
+for(const [key, value] of searchParams) {
+  console.log([key, value])
+  // ["foo", "10"]
+  // ["bar", "2"]
+  // ["foo", "100"]
+}
