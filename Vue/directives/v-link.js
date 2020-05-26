c8d4e7b12336e184947adfc9c4fe3b694162f23b
Violet <violetsshine@gmail.com>
2020-05-26 19:41:02 +0800

    feat: add cursor for  directive

--- Vue/directives/v-link.js
+++ Vue/directives/v-link.js
@@ -1,7 +1,13 @@
+/**
+ * 给任意元素添加`<a>` 链接效果
+ * 该指令适合在某些场景下临时使用
+ * @modifer blank - 是否打开新的窗口
+ */
 export const link = {
   bind(el, binding) {
     el.dataset.href = binding.value;
 
+    el.style.cursor = 'pointer';
     el.__hrefHandler = () => {
       if (binding.modifiers.blank) {
         window.open(el.dataset.href);
