d0a7b0bbf2afad001724952eb80b384fadc953ad
Violet <violetsshine@gmail.com>
2021-04-25 10:20:10 +0800

    feat: add react tpl

---
+++ React/30-days-of-React/ts-tpl/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  state = {
+
+  }
+  render() {
+    return (
+      <div>
+        cool
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
