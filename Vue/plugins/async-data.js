b6acd297b6f16c0425a7b4cb75ab84d068cd2db4
Violet <violetsshine@gmail.com>
2021-05-27 09:43:00 +0800

    feat: add vue async-data mixins

---
+++ Vue/plugins/async-data.js
@@ -0,0 +1,21 @@
+/**
+ * load asyncdata in vue app
+ */
+
+export default {
+  install(Vue) {
+    Vue.mixin({
+      beforeRouteUpdate(to, from, next) {
+        const { asyncData } = this.$options;
+        if (asyncData) {
+          asyncData({
+            store: this.$store,
+            route: to
+          }).then(next).catch(next);
+        } else {
+          next();
+        }
+      }
+    });
+  }
+}
