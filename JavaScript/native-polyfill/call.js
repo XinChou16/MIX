f55e3a2c5524e9ca876066406c37bbf4e17a9fec
Violet <cooolchou@163.com>
2022-06-02 17:19:39 +0800

    feat: call, apply, bind

--- JavaScript/native-polyfill/call.js
+++ JavaScript/native-polyfill/call.js
@@ -16,3 +16,28 @@ Function.prototype.call2 = function(ctx) {
 
   return result;
 }
+
+/**
+ * https://stackoverflow.com/questions/51503894/how-can-create-own-call-function-in-javascript
+ * https://juejin.cn/post/6844903999791955982
+ */
+Function.prototype.call = function (ctx, ...args) {
+  const fn = Symbol();
+  ctx[fn] = this;
+
+  try {
+    const result =  ctx[fn](...args);
+    delete ctx[fn];
+
+    return result;
+  } catch (e) { }
+
+}
+
+Function.prototype.apply = function (ctx, args) {
+  return this.call(ctx, ...args);
+}
+
+Function.prototype.bind = function (ctx, ...args) {
+  return (...args2) => this.call(ctx, ...args, ...args2);
+}
