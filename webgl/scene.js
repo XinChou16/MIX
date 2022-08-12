80009998797cd4de8edca486089c1e70491a8d66
Violet <violetsshine@gmail.com>
2022-08-12 16:49:55 +0800

    feat: add webgl scene file

---
+++ webgl/scene.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three';
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(
+  65,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+let material = new THREE.MeshBasicMaterial( { color: '#96f'} );
+let cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+camera.position.z = 3;
+
+let animate = () => {
+  requestAnimationFrame( animate );
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
